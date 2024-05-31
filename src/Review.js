7940df79b666920d3a946e8283fa7c7888447c0c
Meshach <116318961+Meshach88@users.noreply.github.com>
2024-05-31 07:49:11 +0100

    Fixed prev and next button

--- src/Review.js
+++ src/Review.js
@@ -5,6 +5,31 @@ import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
 const Review = () => {
   const [index, setIndex] = useState(0);
   const { name, image, text, job } = people[index];
+
+  const checkNum = (number) => {
+    if (number > people.length-1) {
+      return 0
+    }
+    if (number < 0) {
+      return people.length-1
+    }
+    else{
+      return number
+    }
+  }
+
+  const nextReview = () => {
+    setIndex((index)=> {
+      let newIndex = index + 1
+      return checkNum(newIndex)
+    })
+  }
+  const prevReview = () => {
+    setIndex((index)=> {
+      let newIndex = index - 1
+      return checkNum(newIndex)
+    })
+  }
   return (
     <article className="review">
       <div className="img-container">
@@ -16,6 +41,11 @@ const Review = () => {
       <h4 className="author">{name}</h4>
       <p className="job">{job}</p>
       <p className="info">{text}</p>
+      <div className="button-container">
+        <button onClick={prevReview} className="prev-btn"><FaChevronLeft/></button>
+        <button onClick={nextReview} className="next-btn"><FaChevronRight/></button>
+      </div>
+        <button className="random-btn">Surprise me</button>
     </article>
   );
 };
